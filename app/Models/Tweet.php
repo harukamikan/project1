4982840f18683aa8e2031fc1870a9049a7596994
harukamikan <s24b2040@bene.fit.ac.jp>
2025-09-06 14:08:01 +0900

    Update Tweet.php
    変更３　Model設定

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    /** @use HasFactory<\Database\Factories\TweetFactory> */
+    
     use HasFactory;
 
-    protected $fillable = ['user_id', 'tweet'];
+    protected $fillable = ['tweet'];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
