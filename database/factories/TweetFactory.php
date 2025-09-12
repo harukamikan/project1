0b125cb7c95f3fdf76bd709417004f4710bdde6d
harukamikan <s24b2040@bene.fit.ac.jp>
2025-09-12 18:23:07 +0900

    テスト

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -2,13 +2,17 @@
 
 namespace Database\Factories;
 
+use App\Models\Tweet;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
  */
 class TweetFactory extends Factory
 {
+    protected $model = Tweet::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,8 @@ class TweetFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+        'user_id' => User::factory(), // UserモデルのFactoryを使用してユーザを生成
+        'tweet' => $this->faker->text(200) // ダミーのテキストデータ
         ];
     }
 }
