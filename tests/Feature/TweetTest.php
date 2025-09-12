c692575d968ebe6bac0ef7f8f7e01fa6ce954814
harukamikan <s24b2040@bene.fit.ac.jp>
2025-09-12 18:31:26 +0900

    テスト

--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -1,7 +1,23 @@
 <?php
 
-it('has tweet page', function () {
-    $response = $this->get('/tweet');
+use App\Models\Tweet;
+use App\Models\User;
 
+it('displays tweets', function () {
+    // ユーザを作成
+    $user = User::factory()->create();
+
+    // ユーザを認証
+    $this->actingAs($user);
+
+    // Tweetを作成
+    $tweet = Tweet::factory()->create();
+
+    // GETリクエスト
+    $response = $this->get('/tweets');
+
+    // レスポンスにTweetの内容と投稿者名が含まれていることを確認
     $response->assertStatus(200);
+    $response->assertSee($tweet->tweet);
+    $response->assertSee($tweet->user->name);
 });
