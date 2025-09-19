749941fee2a15b842f03a784844b1266c29d867f
harukamikan <s24b2040@bene.fit.ac.jp>
2025-09-19 20:10:47 +0900

    ユーザページの実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,6 +24,9 @@
                     <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
                       {{ __('Tweet検索') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('profile.show', auth()->user())" :active="request()->routeIs('profile.show')">
+                      {{ __('マイページ') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -91,6 +94,9 @@
           <x-responsive-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
           {{ __('Tweet検索') }}
          </x-responsive-nav-link>
+          <x-responsive-nav-link :href="route('profile.show', auth()->user())" :active="request()->routeIs('profile.show')">
+        {{ __('マイページ') }}
+      </x-responsive-nav-link>
 
         </div>
 
