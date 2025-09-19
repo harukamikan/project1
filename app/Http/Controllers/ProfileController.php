ae37a5d47607dda82e7b68abdb68e0e8b8f7e0a3
harukamikan <s24b2040@bene.fit.ac.jp>
2025-09-19 20:13:58 +0900

    ユーザページの実装

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -19,6 +20,10 @@ class ProfileController extends Controller
         return view('profile.edit', [
             'user' => $request->user(),
         ]);
+    }
+    public function show(User $user)
+    {
+       return view('profile.show', compact('user'));
     }
 
     /**
