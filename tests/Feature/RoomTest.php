014e22030e2e8d9374ec4b5108051cd22e8b087f
harukamikan <s24b2040@bene.fit.ac.jp>
2025-10-05 17:48:41 +0900

    DM機能

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -1,7 +1,15 @@
-<?php
+use App\Models\User;
+use App\Models\Room;
 
 it('has room page', function () {
-    $response = $this->get('/room');
+    $user1 = User::factory()->create();
+    $user2 = User::factory()->create();
 
-    $response->assertStatus(200);
+    $room = Room::factory()->create();
+    $room->users()->attach([$user1->id, $user2->id]);
+
+    $this->actingAs($user1)
+         ->get(route('room.show', $room))
+         ->assertStatus(200)
+         ->assertSee($user2->name); // 参加者の名前が表示されるか確認
 });
